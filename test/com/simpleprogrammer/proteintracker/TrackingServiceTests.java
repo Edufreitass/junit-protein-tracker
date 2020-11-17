5dc898c94f3703ddec5b4a54745955cb529e123e
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-11-16 21:35:39 -0300

    Creating a simple test

---
+++ test/com/simpleprogrammer/proteintracker/TrackingServiceTests.java
@@ -0,0 +1,14 @@
+package com.simpleprogrammer.proteintracker;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TrackingServiceTests {
+
+	@Test
+	public void testNewTrackingServiceTotalIsZero() {
+		TrackingService service = new TrackingService();
+		assertEquals("Tracking service total was not zero", 0, service.getTotal());
+	}
+
+}
