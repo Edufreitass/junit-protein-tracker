d2b96cec698bb607358aeda6a799ed371e4b9864
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-11-16 21:44:55 -0300

    Creating another simple test

--- test/com/simpleprogrammer/proteintracker/TrackingServiceTests.java
+++ test/com/simpleprogrammer/proteintracker/TrackingServiceTests.java
@@ -10,5 +10,12 @@ public class TrackingServiceTests {
 		TrackingService service = new TrackingService();
 		assertEquals("Tracking service total was not zero", 0, service.getTotal());
 	}
+	
+	@Test
+	public void testWhenAddingProteinTotalIncreaseByThatAmount() {
+		TrackingService service = new TrackingService();
+		service.addProtein(10);
+		assertEquals("Protein amount was not correct", 10, service.getTotal());
+	}
 
 }
