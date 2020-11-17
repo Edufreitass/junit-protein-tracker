e74364ae2c3b85014d021130d6a72562882ae1d8
Eduardo Freitas <eduardoflorencio96@gmail.com>
2020-11-17 10:16:21 -0300

    Using Before

--- test/com/simpleprogrammer/proteintracker/TrackingServiceTests.java
+++ test/com/simpleprogrammer/proteintracker/TrackingServiceTests.java
@@ -3,19 +3,33 @@ package com.simpleprogrammer.proteintracker;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import org.junit.Before;
+
 public class TrackingServiceTests {
+
+	private TrackingService service;
+
+	@Before
+	public void setUp() {
+		System.out.println("Before...");
+		service = new TrackingService();
+	}
 
 	@Test
 	public void testNewTrackingServiceTotalIsZero() {
-		TrackingService service = new TrackingService();
 		assertEquals("Tracking service total was not zero", 0, service.getTotal());
 	}
-	
+
 	@Test
 	public void testWhenAddingProteinTotalIncreaseByThatAmount() {
-		TrackingService service = new TrackingService();
 		service.addProtein(10);
 		assertEquals("Protein amount was not correct", 10, service.getTotal());
+	}
+
+	@Test
+	public void testWhenRemovingProteinTotalRemainsZero() {
+		service.removeProtein(5);
+		assertEquals(0, service.getTotal());
 	}
 
 }
